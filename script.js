4d1291bfd0490a71f93ca7f387ecf207b2d8a39e
yurii.zatochnyi <yurii.zatochnyi@neuraspace.com>
2024-07-02 20:38:57 +0300

    3.1. Intro to JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // console.log('Hello World! ')
 // console.log(2 + 2)
 
-const chalk = cequire( 'chalk');
-console. log(chalk. blue( 'Hello world! ' ) ) ;
+
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
