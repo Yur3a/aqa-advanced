d0e5a33198061d6dc5f4dc1472b4fc0163debb47
yurii.zatochnyi <yurii.zatochnyi@neuraspace.com>
2024-07-10 19:00:25 +0300

    5.1. JavaScript conditions and loops

---
+++ 5.ConditionsAndLoops/task2.js
@@ -0,0 +1,23 @@
+/*
+Завдання 2: Виконайте попереднє завдання але використовуючи конструкцію switch/case
+*/
+
+const averageGrade = 85;
+
+
+switch (true) {
+    case averageGrade < 60:
+        console.log("Незадовільно");
+        break;
+    case averageGrade <= 70:
+        console.log("Задовільно");
+        break;
+    case averageGrade <= 80:
+        console.log("Добре");
+        break;
+    case averageGrade <= 90:
+        console.log("Дуже добре");
+        break;
+    default:
+        console.log("Відмінно");
+}
